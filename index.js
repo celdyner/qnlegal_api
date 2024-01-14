db0284d84e16faa4b1a37ddb5bdd9ae6cc07868a
celdyner <h.oranekwulu@gmail.com>
2024-01-14 09:00:45 +0100

    updated copy

--- index.js
+++ index.js
@@ -40,9 +40,4 @@ const runApp = async () => {
 
 runApp();
 
-// mongoose
-//   .connect(process.env.MONGO_DB)
-//   .then(() => {
-//     app.listen(process.env.PORT, () => console.log("SERVER IS RUNNING NOW!"));
-//   })
-//   .catch((error) => console.log(error));
+
